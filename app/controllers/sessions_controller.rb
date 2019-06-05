772bc0eb54def3441733b53e3fcf3e23010ecf1b
howardbdev <howarddevennish@gmail.com>
2019-06-05 17:59:32 -0400

    adjust sessions controller to use jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,35 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
 
     if @user && @user.authenticate(params[:user][:password])
-      # success!!!!
-      render json: {
-            name: @user.name,
-            email: @user.email,
-            id: @user.id
-          }
+      # if success!!!!
+      # generate a JWT
+      # "beans" should be EVN['SECRET']
+      token = generate_token({id: @user.id})
+      # include that token in the response back to the client
+      # include the user in the reponse as well
+
+      resp = {
+        user: user_serializer(@user),
+        jwt: token
+      }
+
+      render json: resp
     else
       resp = {
         error: "Invalid credentials",
         details: @user.errors.full_messages
       }
-
       render json: resp, status: :unauthorized
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: {
+          user: user_serializer(current_user)
+        }, status: :ok
+    else
+      render json: {error: "No current user"} 
     end
   end
 end
