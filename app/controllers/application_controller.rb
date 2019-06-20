d2d61f6e547de98980ec70e4d9596b24aafef0a3
howardbdev <howarddevennish@gmail.com>
2019-06-20 09:05:29 -0400

    update helper methods to use just session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,10 @@
 class ApplicationController < ActionController::API
-  def user_serializer(user)
-    {
-      name: user.name,
-      email: user.email,
-      id: user.id
-    }
-  end
-
   def current_user
-    begin
-      @current_user ||= User.find(decode_token_and_get_user_id)
-    rescue
-      nil
-    end
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
     !!current_user
   end
 
-  def generate_token(payload)
-    JWT.encode(payload, ENV['JWT_TOKEN_SECRET'])
-  end
-
-  def decode_token_and_get_user_id
-    JWT.decode(request.headers["Authorization"], ENV['JWT_TOKEN_SECRET'])[0]["id"]
-  end
 end
